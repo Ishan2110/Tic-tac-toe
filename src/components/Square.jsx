af0dd9c14d705d144658dd5ef91d9d9d04212a90
ishan21 <guptaishan21@gmail.com>
2021-07-11 15:06:24 +0530

    created board and square components

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Square = props => {
+  const { value } = props;
+  console.log(value);
+  return (
+    <>
+      <button type="button">{value}</button>
+    </>
+  );
+};
+
+export default Square;
